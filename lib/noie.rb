0c4edfeb1c657aa8d902538e91bd0a451a6bc85e
Rob Worley <rob.worley@systino.net>
2011-01-19 13:10:16 -0500

    Added :except and :only options.

--- lib/noie.rb
+++ lib/noie.rb
@@ -8,7 +8,11 @@ module Rack
     end
 
     def call(env)
-      ie_found_in?(env) ? kick_it : @app.call(env)    
+      if enforce_noie?(env) && ie_found_in?(env)
+        kick_it
+      else
+        @app.call(env)
+      end
     end
 
     private
@@ -17,18 +21,42 @@ module Rack
         is_ie?(env['HTTP_USER_AGENT']) and ie_version(env['HTTP_USER_AGENT']) < @options[:minimum] and @options[:redirect] != env['PATH_INFO']
       end
     end
-    
+
     def is_ie?(ua_string)
       # We need at least one digit to be able to get the version, hence the \d
       ua_string.match(/MSIE \d/) ? true : false
     end
-    
+
     def ie_version(ua_string)
       ua_string.match(/MSIE (\S+)/)[1].to_f
     end
 
     def kick_it
       [301, {'Location' => @options[:redirect]}, ['Fail browser is fail']]
+    end
+
+    def enforce_noie?(env)
+      request = Rack::Request.new(env)
+      if @options[:only]
+        match_rule?(request.path, @options[:only])
+      elsif @options[:except]
+        !match_rule?(request.path, @options[:except])
+      else
+        true
+      end
+    end
+
+    # matches a path against a rule
+    # Accepts a string path, a regex matcher or an array of these
+    def match_rule?(path, options)
+      rules = [options].flatten
+      rules.any? do |pattern|
+        if pattern.is_a?(Regexp)
+          path =~ pattern
+        else
+          path[0, pattern.length] == pattern
+        end
+      end
     end
   end
 end
--- test/noie_test.rb
+++ test/noie_test.rb
@@ -12,49 +12,75 @@ class TestApp
 end
 
 class NoieTest < Test::Unit::TestCase
-  
+
   def test_redirects_to_where_it_should_if_ie
     request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org'}))
     response = request.get('/', {'HTTP_USER_AGENT' => 'MSIE 6.0' })
     assert_equal 301, response.status
     assert_equal response.location, 'http://slashdot.org'
   end
-  
+
   def test_redirects_to_where_it_should_if_user_specified_minimum_not_met
     request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org', :minimum => 6.0}))
     response = request.get('/', {'HTTP_USER_AGENT' => 'Mozilla/4.0 (compatible; MSIE 5.5b1; Mac_PowerPC)' })
     assert_equal 301, response.status
     assert_equal response.location, 'http://slashdot.org'
   end
-  
+
   def test_redirects_to_local_urls
     request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => '/foo'}))
-    response = request.get('/foo', {'HTTP_USER_AGENT' => 'MSIE 6.0' })    
+    response = request.get('/foo', {'HTTP_USER_AGENT' => 'MSIE 6.0' })
     assert_equal "Hi Internets!", response.body
   end
-  
+
   def test_allows_if_not_ie
     request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org'}))
     response = request.get('/', {'HTTP_USER_AGENT' => 'Mozilla/5.0'})
     assert_equal "Hi Internets!", response.body
   end
-  
+
   def test_allows_if_UA_version_greater_than_minimum
     request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org'}))
     response = request.get('/', {'HTTP_USER_AGENT' => 'Mozilla/4.0 (compatible; MSIE 8.0; Windows XP)'})
     assert_equal "Hi Internets!", response.body
   end
-  
+
   def test_allows_if_no_UA_version_no_available
     request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org'}))
     response = request.get('/', {'HTTP_USER_AGENT' => 'Mozilla/4.0 (compatible; MSIE l4me; Windows XP)'})
     assert_equal "Hi Internets!", response.body
   end
-  
+
   def test_allows_if_no_user_agent_specified
     request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org'}))
     response = request.get('/')
     assert_equal "Hi Internets!", response.body
   end
-  
+
+  def test_except_option_allows_request_if_path_is_in_list
+    request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org', :except => '/ie6permitted'}))
+    response = request.get('/ie6permitted', {'HTTP_USER_AGENT' => 'MSIE 6.0' })
+    assert_equal "Hi Internets!", response.body
+  end
+
+  def test_except_option_redirects_if_path_is_not_in_list
+    request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org', :except => '/ie6permitted'}))
+    response = request.get('/', {'HTTP_USER_AGENT' => 'MSIE 6.0' })
+    assert_equal 301, response.status
+    assert_equal response.location, 'http://slashdot.org'
+  end
+
+  def test_only_option_allows_request_if_path_is_not_in_list
+    request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org', :only => '/ie6notpermitted'}))
+    response = request.get('/', {'HTTP_USER_AGENT' => 'MSIE 6.0' })
+    assert_equal "Hi Internets!", response.body
+  end
+
+  def test_only_option_redirects_if_path_is_in_list
+    request  = Rack::MockRequest.new(Rack::NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org', :only => '/ie6notpermitted'}))
+    response = request.get('/ie6notpermitted', {'HTTP_USER_AGENT' => 'MSIE 6.0' })
+    assert_equal 301, response.status
+    assert_equal response.location, 'http://slashdot.org'
+  end
+
 end
